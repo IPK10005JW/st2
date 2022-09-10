43218e675031c5d3ccdb3eba407a6efb8b1a0a40
IPK10005JW <113221679+IPK10005JW@users.noreply.github.com>
2022-09-10 11:36:14 +0800

    Add files via upload

---
+++ 10-app2.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+
+# title
+st.title("My favorites")
+
+# 3 columns that display your favorite songs from youtube
+
+
+
+
+
+# 3 columns that display your photo of favorite foods.
+
+
+
+
+# balloon or snowflake
+
+
+
+# caption by your name
+st.caption("Developed by Wawiwu")
